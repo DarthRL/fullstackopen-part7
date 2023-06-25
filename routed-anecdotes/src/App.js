0e3cd0b6e89d99be455fb2f748190312ea9c2f24
DarthRL <darthrlu@gmail.com>
2023-06-25 22:08:49 +0800

    7.6

--- routed-anecdotes/src/App.js
+++ routed-anecdotes/src/App.js
@@ -58,9 +58,9 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const content = useField('text')
-  const author = useField('text')
-  const info = useField('text')
+  const [content, resetContent] = useField('text')
+  const [author, resetAuthor] = useField('text')
+  const [info, resetInfo] = useField('text')
 
 
   const handleSubmit = (e) => {
@@ -75,9 +75,9 @@ const CreateNew = (props) => {
 
   const handleReset = (e) => {
     e.preventDefault()
-    content.reset()
-    author.reset()
-    info.reset()
+    resetContent()
+    resetAuthor()
+    resetInfo()
   }
 
   return (
--- routed-anecdotes/src/hooks/index.js
+++ routed-anecdotes/src/hooks/index.js
@@ -11,10 +11,12 @@ export const useField = (type) => {
     setValue('')
   }
 
-  return {
-    type,
-    value,
-    onChange,
+  return [
+    {
+      type,
+      value,
+      onChange,
+    },
     reset
-  }
+  ]
 }
